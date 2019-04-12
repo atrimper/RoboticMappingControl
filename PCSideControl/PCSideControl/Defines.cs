0a627daac2c8bbdad3cd18624c96c62909da73a3
Andrew Trimper <liv4waterville@gmail.com>
2019-04-12 16:41:34 -0400

    Added generic functionality to map plotting

---
+++ PCSideControl/PCSideControl/Defines.cs
@@ -0,0 +1,11 @@
+﻿namespace PCSideControl {
+    class Defines {
+        public static int MAP_X = 0;
+        public static int MAP_Y = 0;
+        public static double MAX_DISTANCE = 10.0;
+        public static int MAP_SIZE = 200;
+        public static int ROBOT_SIZE = 10;
+        public static int DOT_SIZE = 2;
+        public static double DISTANCE_TO_PIXELS = (MAP_SIZE / 2) / MAX_DISTANCE;
+    }
+}
--- PCSideControl/PCSideControl/Form1.cs
+++ PCSideControl/PCSideControl/Form1.cs
@@ -20,29 +20,37 @@ namespace PCSideControl {
             return new Rectangle(center.X - size.Width / 2, center.Y - size.Height / 2, size.Width, size.Height);
         }
 
-        private void printMap(Graphics g, Point center, Size size) {
-            Random r = new Random();
-            double xScale = 0;
-            double yScale = 0;
+        private List<Tuple<double, double>> readMapData() {
+            List<Tuple<double, double>> list = new List<Tuple<double, double>>();
             for(int i = 0; i < 360; i++) {
-                xScale = 50 * (((r.NextDouble() - 0.5) * 0.5) + 1);
-                int x = (int) (xScale * Math.Cos(i * Math.PI / 180));
-                yScale = 50 * (((r.NextDouble() - 0.5) * 0.5) + 1);
-                int y = (int) (yScale * Math.Sin(i * Math.PI / 180));
+                list.Add(new Tuple<double, double>(11, i));
+            }
+            return list;
+        }
+
+        private void printMap(Graphics g, Point center, Size size) {
+            List<Tuple<double, double>> polarCoordinates = readMapData();
+            double distance;
+            double angle;
+            foreach (Tuple<double, double> t in polarCoordinates) {
+                distance = t.Item1;
+                angle = t.Item2;
+                int x = (int) (distance * Defines.DISTANCE_TO_PIXELS * Math.Cos(angle * Math.PI / 180));
+                int y = (int) (distance * Defines.DISTANCE_TO_PIXELS * Math.Sin(angle * Math.PI / 180));
                 Point wall = new Point(center.X + x, center.Y + y);
-                if(xScale < 100 && yScale < 100) {
-                    g.FillRectangle(Brushes.Black, generateCenterPointRectangle(wall, new Size(2, 2)));
+                if (Math.Abs(x) < Defines.MAP_SIZE / 2 && Math.Abs(y) < Defines.MAP_SIZE / 2) {
+                    g.FillRectangle(Brushes.Black, generateCenterPointRectangle(wall, new Size(Defines.DOT_SIZE, Defines.DOT_SIZE)));
                 }
             }
         }
 
         private void form1_Paint(object sender, PaintEventArgs e) {
             Graphics g = this.CreateGraphics();
-            Point topLeft = new Point(0, 0);
-            Point mapCenter = new Point(topLeft.X + 100, topLeft.Y + 100);
-            Size mapSize = new Size(200, 200);
+            Point topLeft = new Point(Defines.MAP_X, Defines.MAP_Y);
+            Point mapCenter = new Point(topLeft.X + Defines.MAP_SIZE / 2, topLeft.Y + Defines.MAP_SIZE / 2);
+            Size mapSize = new Size(Defines.MAP_SIZE, Defines.MAP_SIZE);
             g.DrawRectangle(Pens.Black, generateCenterPointRectangle(mapCenter, mapSize));
-            g.FillRectangle(Brushes.Red, generateCenterPointRectangle(mapCenter, new Size(10, 10)));
+            g.FillRectangle(Brushes.Red, generateCenterPointRectangle(mapCenter, new Size(Defines.ROBOT_SIZE, Defines.ROBOT_SIZE)));
             printMap(g, mapCenter, mapSize);
         }
     }
