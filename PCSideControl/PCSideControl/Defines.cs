99384d0e3d63720cf6b79e9cf85a0c9991975623
Andrew Trimper <liv4waterville@gmail.com>
2019-04-15 13:46:11 -0400

    Separated writeMap() and readData() functions

--- PCSideControl/PCSideControl/Defines.cs
+++ PCSideControl/PCSideControl/Defines.cs
@@ -3,7 +3,7 @@
         public static int MAP_X = 0;
         public static int MAP_Y = 0;
         public static double MAX_DISTANCE = 10.0;
-        public static int MAP_SIZE = 200;
+        public static int MAP_SIZE = 300;
         public static int ROBOT_SIZE = 10;
         public static int DOT_SIZE = 2;
         public static double DISTANCE_TO_PIXELS = (MAP_SIZE / 2) / MAX_DISTANCE;
