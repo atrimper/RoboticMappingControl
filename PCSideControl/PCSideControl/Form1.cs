7a985e3ed969babe1a36b0e5d8c39d4df91cc470
Shivchopra5 <schopra37@gatech.edu>
2019-04-15 13:37:09 -0400

    Added button click event and mouse click event for serial

--- PCSideControl/PCSideControl/Form1.cs
+++ PCSideControl/PCSideControl/Form1.cs
@@ -10,6 +10,10 @@ using System.Windows.Forms;
 
 namespace PCSideControl {
     public partial class Form1 : Form {
+        Graphics g;
+        int[] arr;
+        int numPoints = 10;
+        double angle;
         public Form1() {
             InitializeComponent();
             this.Width = 750;
@@ -44,14 +48,64 @@ namespace PCSideControl {
             }
         }
 
+        private void print_points(Point center)
+        {
+            angle = 360 / numPoints;
+
+        }
+
         private void form1_Paint(object sender, PaintEventArgs e) {
-            Graphics g = this.CreateGraphics();
+            this.g = this.CreateGraphics();
             Point topLeft = new Point(Defines.MAP_X, Defines.MAP_Y);
             Point mapCenter = new Point(topLeft.X + Defines.MAP_SIZE / 2, topLeft.Y + Defines.MAP_SIZE / 2);
             Size mapSize = new Size(Defines.MAP_SIZE, Defines.MAP_SIZE);
             g.DrawRectangle(Pens.Black, generateCenterPointRectangle(mapCenter, mapSize));
             g.FillRectangle(Brushes.Red, generateCenterPointRectangle(mapCenter, new Size(Defines.ROBOT_SIZE, Defines.ROBOT_SIZE)));
             printMap(g, mapCenter, mapSize);
+            // this.Cursor = new Cursor(Cursor.Current.Handle);
+            //Cursor.Position = new Point(Cursor.Position.X - 50, Cursor.Position.Y - 50);
+            //while (true) {
+            //    g.DrawRectangle(Pens.Black, generateCenterPointRectangle(Cursor.Position, new Size(3, 3)));
+            //}
+        }
+
+        private void mouse_click(object sender, MouseEventArgs e)
+        {
+            this.g.DrawRectangle(Pens.Black, generateCenterPointRectangle(new Point(e.X,e.Y), new Size(3,3)));
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            //Console.WriteLine(arr);
+            serialPort1.Open();
+            arr = new int[numPoints];
+            bool toCont = true;
+            bool recieveData = false;
+            int i = 0;
+            while(toCont == true)
+            {
+                try
+                {
+                    this.arr[i] = serialPort1.ReadChar();
+                    recieveData = true;
+                    i++;
+                    
+                }
+                catch (System.IO.IOException)
+                {
+                    toCont = false;
+                }
+            }
+            serialPort1.DiscardInBuffer();
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            serialPort1.Close();
+            if (recieveData == true)
+            {
+                drawPoints();
+            }
         }
     }
 }
--- robot/README.txt
+++ robot/README.txt
@@ -1,2 +1,1 @@
 This directory is for the robot control and communication code
-Test
